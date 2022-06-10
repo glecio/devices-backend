64a5e235c08b715a3856bed2f02ef120fa7e95a0
Glecio Santos <glecio.santos@gmail.com>
2022-06-10 14:48:59 -0300

    Make Models and Migrations

---
+++ src/database/migrations/2022_06_10_171604_create_devices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->enum('voltage', ['110', '220', 'auto']);
+            $table->unsignedBigInteger('brand_id');
+            $table->timestamps();
+        });
+
+        Schema::table('devices', function (Blueprint $table) {
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
