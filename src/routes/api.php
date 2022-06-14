09020b1a5c0928f9060f4984d1a98c6a296c6919
Glecio Santos <glecio.santos@gmail.com>
2022-06-14 20:18:21 -0300

    Api routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('brands', \App\Http\Controllers\Api\BrandController::class );
+Route::apiResource('devices', \App\Http\Controllers\Api\DeviceController::class );
